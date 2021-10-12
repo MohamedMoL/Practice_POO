78c54a95b9df73d45f7127cc750ff39d3627e611
MohamedML <mohamed.moussati.lamhamdi@gmail.com>
2021-10-12 18:40:52 +0100

    Creating the files and folders

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Se ha ejecutado el main");
+
+        Persona persona = new Persona("Mohamed");
+
+        System.out.println(persona.getNombre());
+    }
+}
---
+++ java/Persona.java
@@ -0,0 +1,12 @@
+public class Persona{
+
+    final private String nombre;
+
+    public Persona(final String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+}
